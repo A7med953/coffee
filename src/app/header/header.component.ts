4fa9d27d7be187a40982a26510d152a39eca7ba8
ahmed elsayed <a7md1elsayed@gmail.com>
2025-09-16 20:52:30 +0300

    Initial commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet,RouterModule, ],
+  imports: [RouterModule, ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
